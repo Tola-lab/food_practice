5811194870535055c7b4d5038adaa9d7cc4fa144
Lisa <example@gmail.com>
2023-06-19 13:26:54 +0600

    add tabs

---
+++ js/script.js
@@ -0,0 +1,37 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabContent () {   // скрываем все не нужные пока табы
+        tabsContent.forEach(item => {
+            item.style.display = 'none';
+        });
+
+        tabs.forEach(item => {   // удаляем класс активности, когда скрываем не нужные табы
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent (i = 0) { // показываем нужный нам таб. i = 0 – это дефолтный показ первого слайда, если при вызове функции не передаём аргумент
+        tabsContent[i].style.display = 'block';
+        tabs[i].classList.add('tabheader__item_active');  // добавляем класс активности
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (e) => {  // создаём делегирование события 
+        const target = e.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => { // i - номер элемента по порядку
+                if (target == item ) { // если таргет будет совпадать с элементом, который мы сейчас перебираем
+                    hideTabContent(); // то скрываем те табы, которые не нужны
+                    showTabContent(i); // и показываем тот, который нужен. i – это таб по порядку (на который кликнули)
+                }
+            });
+        }
+    }); 
+});
