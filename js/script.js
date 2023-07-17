0d2539bbf4322efb570ff4e54fdcc3c238e7e4a0
Lisa <example@gmail.com>
2023-07-17 16:08:41 +0600

    add npm & json-server nvfjn

--- js/script.js
+++ js/script.js
@@ -299,4 +299,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 4000);
     }
 
+    fetch('http://localhost:3000/menu')
+    .then(data => data.json())
+    .then(res => console.log(res));
 });
